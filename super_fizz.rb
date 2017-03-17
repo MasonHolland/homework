52ee69ed45cf58ceb02446c963ab692f49cd8112
MasonHolland <masontholland@gmail.com>
2017-03-16 21:01:23 -0600

    super_fizz not working and updated bad_connection, still not working

---
+++ super_fizz.rb
@@ -0,0 +1,21 @@
+puts "Lets play some SuperFizz"
+
+puts "enter a number: "
+
+number = gets.chomp
+
+if number >
+
+if number % 3 == 0
+  puts "Fizz"
+end
+
+if number % 5 == 0
+  puts "Buzz"
+end
+
+if number % 7 == 0
+  puts "Super"
+end
+
+puts number
